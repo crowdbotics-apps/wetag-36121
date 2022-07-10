9e939dce1fe7978b04156a53fd08135c58b7f174
Crowdbotics <team@crowdbotics.com>
2022-07-10 20:36:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wetag_36121/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wetag_36121;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wetag_36121";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wetag_36121",
+  url: "https://wetag_36121.botics.co",
+  api: "https://wetag_36121.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
